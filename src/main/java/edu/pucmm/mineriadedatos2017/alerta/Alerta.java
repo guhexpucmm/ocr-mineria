aa4a10498f56fda6d1f10d555de7a2b85e7e1157
Gustavo Jose Henríquez Contreras <gustavojoseh@gmail.com>
2017-07-14 14:17:13 -0400

    Diseño de la pantalla principal cambiado y modificado.

--- src/main/java/edu/pucmm/mineriadedatos2017/alerta/Alerta.java
+++ src/main/java/edu/pucmm/mineriadedatos2017/alerta/Alerta.java
@@ -1,17 +1,21 @@
 package edu.pucmm.mineriadedatos2017.alerta;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
 
 public class Alerta extends Alert{
     private String titulo;
     private String header;
     private String contenido;
+    private DialogPane dialogPane;
 
     public Alerta(String titulo, String header, String contenido, AlertType alertType) {
         super(alertType);
         this.titulo = titulo;
         this.header = header;
         this.contenido = contenido;
+        dialogPane = getDialogPane();
+        dialogPane.getStylesheets().add("/css/main.css");
     }
 
     public Alerta aviso() {
--- src/main/java/edu/pucmm/mineriadedatos2017/controladora/PantallaPrincipalControladora.java
+++ src/main/java/edu/pucmm/mineriadedatos2017/controladora/PantallaPrincipalControladora.java
@@ -1,30 +1,23 @@
 package edu.pucmm.mineriadedatos2017.controladora;
 
-import edu.pucmm.mineriadedatos2017.alerta.Alerta;
-import edu.pucmm.mineriadedatos2017.util.VentanaUtil;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
 import javafx.scene.layout.*;
-import javafx.stage.Stage;
-import org.controlsfx.control.textfield.CustomTextField;
+import javafx.scene.paint.Color;
 
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PantallaPrincipalControladora implements Initializable {
 
-    private StringProperty archivo;
+    private GraphicsContext graphicsContext;
+
 
     @FXML
     private StackPane stackPane;
@@ -33,37 +26,56 @@ public class PantallaPrincipalControladora implements Initializable {
     private VBox vBox;
 
     @FXML
+    private Button btnBorrar;
+
+    @FXML
+    private Pane pane;
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
     private HBox hBox;
 
     @FXML
     private Button btnEntrenarModelo;
 
     @FXML
+    private Button btnSuperEntrenamiento;
+
+    @FXML
     private Button btnReconocerLetra;
 
-    @FXML
-    private HBox hBox2;
-
-    @FXML
-    private CustomTextField txtField;
-
-    @FXML
-    private Button btnAnalizar;
 
     public PantallaPrincipalControladora() {
-        archivo = new SimpleStringProperty("");
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        txtField.textProperty().bindBidirectional(archivo);
-
         setImage();
+
+        graphicsContext = canvas.getGraphicsContext2D();
+        graphicsContext.setStroke(Color.BLACK);
+        graphicsContext.setLineWidth(5);
     }
 
     @FXML
-    void btnEjecutarModeloClick(ActionEvent event) {
-        ejecutarModelo();
+    void btnBorrarClick(ActionEvent event) {
+        borrar();
+    }
+
+    @FXML
+    void canvasMousePressed(MouseEvent event) {
+        graphicsContext.beginPath();
+        graphicsContext.lineTo(event.getX(), event.getY());
+        graphicsContext.stroke();
+    }
+
+    @FXML
+    void canvasMouseDragged(MouseEvent event) {
+        graphicsContext.lineTo(event.getX(), event.getY());
+        graphicsContext.stroke();
     }
 
     @FXML
@@ -72,106 +84,17 @@ public class PantallaPrincipalControladora implements Initializable {
     }
 
     @FXML
-    void btnAnalizarClick(ActionEvent event) {
-        analizar();
-    }
-
-    @FXML
-    void txtFieldDragDetected(MouseEvent event) {
+    void btnSuperEntrenamientoClick(ActionEvent event) {
 
     }
 
     @FXML
-    void txtFieldDragDone(DragEvent event) {
-
-    }
-
-    @FXML
-    void txtFieldDragDropped(DragEvent event) {
-        Dragboard dragboard = event.getDragboard();
-
-        if (dragboard.hasFiles()) {
-            archivo.set(dragboard.getFiles().get(0).getAbsolutePath());
-        }
-
-        ((Stage) btnEntrenarModelo.getScene().getWindow()).toFront();
-
-        event.setDropCompleted(true);
-
-        event.consume();
-    }
-
-    @FXML
-    void txtFieldDragEntered(DragEvent event) {
-        if (event.getDragboard().hasFiles())
-            event.acceptTransferModes(TransferMode.COPY);
-    }
-
-    @FXML
-    void txtFieldDragExited(DragEvent event) {
-
-    }
-
-    @FXML
-    void txtFieldDragOver(DragEvent event) {
-        if (event.getDragboard().hasFiles())
-            event.acceptTransferModes(TransferMode.COPY);
-    }
-
-
-
-    @FXML
-    void vBoxDragDetected(MouseEvent event) {
-
-    }
-
-    @FXML
-    void vBoxDragDone(DragEvent event) {
-
-    }
-
-    @FXML
-    void vBoxDragDropped(DragEvent event) {
-        Dragboard dragboard = event.getDragboard();
-
-        if (dragboard.hasFiles()) {
-            archivo.set(dragboard.getFiles().get(0).getAbsolutePath());
-        }
-
-        ((Stage) btnEntrenarModelo.getScene().getWindow()).toFront();
-
-        event.setDropCompleted(true);
-
-        event.consume();
-    }
-
-    @FXML
-    void vBoxDragEntered(DragEvent event) {
-        if (event.getDragboard().hasFiles())
-            event.acceptTransferModes(TransferMode.COPY);
-    }
-
-    @FXML
-    void vBoxDragExited(DragEvent event) {
-
-    }
-
-    @FXML
-    void vBoxDragOver(DragEvent event) {
-        if (event.getDragboard().hasFiles())
-            event.acceptTransferModes(TransferMode.COPY);
-    }
-
-    private void ejecutarModelo() {
+    void btnEntrenarModeloClick(ActionEvent event) {
 
     }
 
     private void reconocerLetra() {
-        if (new File(archivo.get()).exists()) {
-            new VentanaUtil(new Stage(), "Resultado", "/vista/Resultado.fxml").propiedades().setClosable().abrir();
-        } else {
-            new Alerta("Aviso!", "Archivo invalido no encontrado.", "Esta tratando de analizar un archivo que no fue encontrado o es invalido.", Alert.AlertType.WARNING).aviso().mostrar();
-        }
+
     }
 
     private void analizar() {
@@ -182,5 +105,9 @@ public class PantallaPrincipalControladora implements Initializable {
         Image image = new Image("/fotos/fondo_principal.png");
         BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true));
         stackPane.setBackground(new Background(backgroundImage));
+    }
+
+    private void borrar() {
+        graphicsContext.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 }
